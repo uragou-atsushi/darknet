09c9d1fea38a2c143c0c1d2db3814c621d841a17
浦郷敦 <urago@hanlabo.co.jp>
2020-07-16 13:41:09 +0900

    no message

--- python/divide.py
+++ python/divide.py
@@ -7,14 +7,16 @@ print(current_dir)
 
 current_dir = '../data/whill'
 g_colab_full_path='/cotent/darknet/data/whill'
-
+print("test")
 # Percentage of images to be used for the test set
 percentage_test = 10;
 
 # Create and/or truncate train.txt and test.txt
 file_train = open('whill-train.txt', 'w')  
+print(file_train)
 file_test = open('whill-test.txt', 'w')# Populate train.txt and test.txt
 counter = 1  
+print("test")
 index_test = round(100 / percentage_test)  
 for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):  
     title, ext = os.path.splitext(os.path.basename(pathAndFilename))
